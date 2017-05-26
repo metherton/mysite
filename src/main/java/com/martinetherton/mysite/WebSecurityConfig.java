8cdf9fce76ed57b312bc59886381326833ce6515
metherton <martinetherton63@gmail.com>
2017-05-26 20:12:55 +0200

    add security

--- src/main/java/com/martinetherton/mysite/WebSecurityConfig.java
+++ src/main/java/com/martinetherton/mysite/WebSecurityConfig.java
@@ -21,30 +21,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         webSecurity
                 .ignoring()
                 .antMatchers("/*.js");
+//        webSecurity.ignoring()
+//                .antMatchers("/login");
 
 
-
-//        webSecurity
-//                .ignoring()
-//                // All of Spring Security will ignore the requests
-//                .antMatchers(HttpMethod.POST, "/assignment_callback.json")
-//        ;
-
-//        webSecurity
-//                .ignoring()
-//                // All of Spring Security will ignore the requests
-//                .antMatchers(HttpMethod.POST, "/enqueue_call")
-//        ;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/signup","/about", "/", "/service-worker.js").permitAll()
+                .antMatchers("/home", "/signup","/about", "/", "/service-worker.js").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .permitAll()
                 .and()
                 .httpBasic();
     }
--- src/main/java/com/martinetherton/mysite/web/UserController.java
+++ src/main/java/com/martinetherton/mysite/web/UserController.java
@@ -4,7 +4,9 @@ import com.martinetherton.mysite.models.User;
 import com.martinetherton.mysite.models.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -85,6 +87,12 @@ public class UserController {
     }
 
     // Private fields
+
+    @RequestMapping(value="/login", method= RequestMethod.GET)
+    public String loadForm(Model model) {
+        model.addAttribute("user", new User());
+        return "/login";
+    }
 
     @Autowired
     private UserDao userDao;
